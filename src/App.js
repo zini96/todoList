cceaef8bdccc2f7c73bbaf1e2ec724f54736f4e3
zini <rkdtnwls7942@naver.com>
2022-01-03 14:55:08 +0900

    app 수정

--- src/App.js
+++ src/App.js
@@ -5,13 +5,35 @@ import TodoList from './components/TodoList';
 import TodoInsert from './components/TodoInsert';
 import {useState, useRef} from "react";
 
+const GlobalStyle = createGlobalStyle`
+body{
+  background:#e9ecef;
+}
+`;
+
 function App() {
   //항목들 상태관리
-  const [todoList,settodoList] = useState([]);
+  const [todoList,settodoList] = useState([
+    {
+      id: 1,
+      text : "프로젝트 관리하기",
+      idDone: false
+    },
+    {
+      id: 2,
+      text : "스타일 컴포넌트",
+      idDone: false
+    },
+    {
+      id: 3,
+      text : "스타일 컴포넌트",
+      idDone: false
+    }
+  ]);
   //인풋입력값 상태관리
   const [desc, setDesc] = useState("");
   //항목id관리
-  const idNum = useRef(1);
+  const idNum = useRef(4);
 
   function onChange(e){
     const inputValue = e.target.value;
@@ -40,17 +62,16 @@ function App() {
   }
   //해당 id항목 isDone 반전
   function onToggle(id){
-    settodoList(todoList.map(todo=>todo.id === id? {...todo.idDone = !idDone} : todo))
+    settodoList(todoList.map(todo=>todo.id === id? {...todo,idDone : !todo.idDone} : todo))
   }
 
 
-  const GlobalStyle = createGlobalStyle`
-    body{
-      background:#e9ecef;
-    }
-  `;
   return (
     <div className="App">
+      <div>
+        <h1>안녕하세요</h1>
+        <p>원격저장소 테스트입니다</p>
+      </div>
       <GlobalStyle/>
       <TodoTemplate>
         <TodoHeader todoList={todoList}/>
